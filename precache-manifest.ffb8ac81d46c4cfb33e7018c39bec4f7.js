6b774fd51cf13ec36f06a5042326cbd329e8a205
Nastia Krat <nastyakpi1995@gmail.com>
2019-12-23 15:07:14 +0200

    Updates

---
+++ precache-manifest.ffb8ac81d46c4cfb33e7018c39bec4f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1993b368abb2fb2cca97dea84aad16",
+    "url": "/bitCoin/index.html"
+  },
+  {
+    "revision": "c931508871d94e77e150",
+    "url": "/bitCoin/static/css/main.be8787b2.chunk.css"
+  },
+  {
+    "revision": "fddd66a18804abf7c277",
+    "url": "/bitCoin/static/js/2.ac9062e3.chunk.js"
+  },
+  {
+    "revision": "c931508871d94e77e150",
+    "url": "/bitCoin/static/js/main.bf07cf5c.chunk.js"
+  },
+  {
+    "revision": "7091efc69b8d909657de",
+    "url": "/bitCoin/static/js/runtime-main.c777f358.js"
+  }
+]);
